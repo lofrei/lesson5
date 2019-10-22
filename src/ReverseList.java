042d35c6d44805bfa1cf9f3f1d1c2659b72e3d07
Lotte <lotte.freischlad@web.de>
2019-10-22 19:01:34 +0200

    added ReverseList.java

---
+++ src/ReverseList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ReverseList {
+    public static void main(String[] args) {
+        List<String> namelist = new ArrayList<>();
+        System.out.println("Please enter names, enter empty line to stop: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        while (!name.equals("")) {
+            namelist.add(name);
+            name = scanner.nextLine();
+        }
+        System.out.println("Original Namelist: " + namelist);
+        System.out.println("Namelist reversed manually: ");
+        for (int i = namelist.size() - 1; i >= 0; i--) {
+            System.out.println(namelist.get(i));
+        }
+        Collections.reverse(namelist);
+        System.out.println("Namelist reversed using Collections method: " + namelist);
+        Set<String> emails = new HashSet<>();
+    }
+}
